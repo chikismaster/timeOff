6495cbfb2bb07f2a5849624b11bdc5013aa82fcd
marquitobb <maca9608@gmail.com>
2020-03-18 18:01:57 -0600

    se cambio a db online clever-cloud

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -9,11 +9,17 @@ import modelos.login;
 public class conexion {
     
     private static Connection conn;
+    //conexion local
+    /*
     private static final String driver = "com.mysql.jdbc.Driver";
     private static final String user = "root";
     private static final String password = "";
     private static final String url = "jdbc:mysql://localhost:3306/db_timeoff";
-
+    */
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String user = "uwxe4zfnuzulaykh";
+    private static final String password = "nOwtgWhZa16iA9deHx4D";
+    private static final String url = "jdbc:mysql://b7sis9pkpmxjxql3njhz-mysql.services.clever-cloud.com/b7sis9pkpmxjxql3njhz";
     public static boolean cam = false;
     
     public conexion() {
